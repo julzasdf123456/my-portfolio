98b1298592fd1df5b44ec4b4e1b7c2c5393ca2e8
Julio Lopez <julzasdf123456@gmail.com>
2025-01-07 19:38:13 +0800

    added 3d

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,6 +1,12 @@
 import FrontImage from "../assets/imgs/front-blob.png";
+import { Canvas, useLoader } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { GLTFLoader, RGBELoader } from "three/examples/jsm/Addons.js";
 
 function Landing() {
+  const earth = useLoader(GLTFLoader, "/3ds/spot-walk/spot.gltf");
+  const hdr = useLoader(RGBELoader, "/3ds/spot-walk/hdri.hdr");
+
   return (
     <>
       <div className="h-full lg:h-screen lg:min-h-[700px] md:h-screen md:min-h-[740px] flex flex-col md:flex-row lg:flex-row">
@@ -21,13 +27,13 @@ function Landing() {
             </h1>
 
             <p className="pt-4 text-white text-opacity-75">
-              Seasoned Full-Stack Software Engineer with 9+ years of experience
-              in Web, Android, API Integration, Desktop application development,
-              and Data Analytics. Skilled in database management, including MS
-              SQL Server, MySQL, PostgreSQL, and Oracle, with 7+ years managing
-              SQL databases with replication to enhance data integrity and
-              redundancy. Proven success in delivering quality solutions for
-              local and international enterprises.
+              With 9+ years of experience in Web, Android, API Integration,
+              Desktop application development, and Data Analytics. Skilled in
+              database management, including MS SQL Server, MySQL, PostgreSQL,
+              and Oracle, with 7+ years managing SQL databases with replication
+              to enhance data integrity and redundancy. Proven success in
+              delivering quality solutions for local and international
+              enterprises.
             </p>
             <p className="pt-3 text-white text-opacity-75">
               Currently based in the Philippines. Available to relocate to any
@@ -52,7 +58,46 @@ function Landing() {
         </div>
       </div>
 
-      <div className="h-full lg:h-screen md:h-screen flex flex-col md:flex-row lg:flex-row bg-cyan-600 rounded-tr-[20%] rounded-bl-[20%] p-5"></div>
+      <div className="h-full lg:h-screen md:h-screen lg:min-h-[500px] md:h-screen md:min-h-[100px] flex flex-col md:flex-row lg:flex-row bg-cyan-600 rounded-tr-[200px] rounded-bl-[200px] py-3 bg-opacity-50">
+        <div className="flex-1 h-full px-10 md:px-5 lg:px-12 py-4"></div>
+
+        <div className="flex-none flex items-center w-screen md:w-6/12 lg:w-7/12 px-2 md:px-5 lg:px-12 lg:py-8">
+          <div className="w-full h-[90vh] bg-gray-400 rounded-l-[80px] rounded-br-[80px] rounded-tr-[160px] bg-gradient-to-br from-indigo-900 via-purple-900 to-pink-900">
+            <Canvas
+              frameloop="demand"
+              camera={{
+                position: [-15, 0, 20],
+                fov: 45,
+                near: 0.1,
+                far: 200,
+              }}
+              gl={{
+                antialias: true,
+                alpha: true,
+                powerPreference: "high-performance",
+              }}
+              style={{ background: "transparent" }}
+            >
+              <ambientLight intensity={2} />
+              <pointLight intensity={50} position={[10, 10, 10]} />
+              <pointLight intensity={50} position={[60, 60, 10]} />
+
+              {/* Custom Primitive */}
+              <mesh>
+                <OrbitControls
+                  autoRotate={false}
+                  enableZoom={true}
+                  maxPolarAngle={Math.PI / 2}
+                  minPolarAngle={Math.PI / 2}
+                  enablePan={true}
+                />
+                <primitive object={earth.scene} scale={2.5} />
+                <meshStandardMaterial color="orange" />
+              </mesh>
+            </Canvas>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
