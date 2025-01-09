3e6c66d864249f798976241e49c4ee6b72a30ed4
Julio Lopez <julzasdf123456@gmail.com>
2025-01-09 13:51:23 +0800

    addons

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,4 +1,5 @@
 import FrontImage from "../assets/imgs/front-blob.png";
+
 import c from "../assets/logos/lang/c.png";
 import csharp from "../assets/logos/lang/csharp.png";
 import javascript from "../assets/logos/lang/javascript.png";
@@ -8,6 +9,24 @@ import php from "../assets/logos/lang/php.png";
 import java from "../assets/logos/lang/java.svg";
 import python from "../assets/logos/lang/python.png";
 import flutter from "../assets/logos/lang/flutter.png";
+import ts from "../assets/logos/lang/typescript.png";
+
+import react from "../assets/logos/frameworks/react.webp";
+import vue from "../assets/logos/frameworks/vue.png";
+import laravel from "../assets/logos/frameworks/laravel.png";
+import node from "../assets/logos/frameworks/node.png";
+import angular from "../assets/logos/frameworks/angular.webp";
+import jquery from "../assets/logos/frameworks/jquery.webp";
+import wordpress from "../assets/logos/frameworks/wordpress.png";
+import bootstrap from "../assets/logos/frameworks/bootstrap.png";
+import tailwind from "../assets/logos/frameworks/tailwind.png";
+
+import mssql from "../assets/logos/db/mssql.svg";
+import mysql from "../assets/logos/db/mysql.png";
+import postgresql from "../assets/logos/db/postgresql.png";
+import mongodb from "../assets/logos/db/mongodb.png";
+import maridb from "../assets/logos/db/mariadb.png";
+import sqlite from "../assets/logos/db/sqlite.svg";
 // import { useFrame } from "@react-three/fiber";
 // import { useGLTF } from "@react-three/drei";
 // import React, { useRef } from "react";
@@ -69,6 +88,120 @@ function Landing() {
   //   ) : null;
   // };
 
+  const languages = [
+    {
+      name: "Javascript",
+      logo: javascript,
+      cssClass: "lg:h-[90px] md:h-[70px] h-[60px]",
+    },
+    { name: "CSS 3", logo: css, cssClass: "lg:h-[90px] md:h-[70px] h-[60px]" },
+    {
+      name: "HTML 5",
+      logo: html,
+      cssClass: "lg:h-[90px] md:h-[70px] h-[60px]",
+    },
+    {
+      name: "Typescript",
+      logo: ts,
+      cssClass: "lg:h-[70px] md:h-[60px] h-[60px]",
+    },
+    { name: "PHP", logo: php, cssClass: "lg:h-[65px] md:h-[70px] h-[50px]" },
+    { name: "Java", logo: java, cssClass: "lg:h-[90px] md:h-[70px] h-[60px]" },
+    {
+      name: "Python",
+      logo: python,
+      cssClass: "lg:h-[90px] md:h-[70px] h-[60px]",
+    },
+    {
+      name: "Flutter/Dart",
+      logo: flutter,
+      cssClass: "lg:h-[90px] md:h-[70px] h-[60px]",
+    },
+    { name: "C", logo: c, cssClass: "lg:h-[90px] md:h-[70px] h-[60px]" },
+    { name: "C#", logo: csharp, cssClass: "lg:h-[90px] md:h-[70px] h-[60px]" },
+  ];
+
+  const frameworks = [
+    {
+      name: "React.JS",
+      logo: react,
+      cssClass: "lg:h-[80px] md:h-[60px] h-[55px]",
+    },
+    {
+      name: "Vue.JS",
+      logo: vue,
+      cssClass: "lg:h-[80px] md:h-[60px] h-[55px]",
+    },
+    {
+      name: "Laravel",
+      logo: laravel,
+      cssClass: "lg:h-[80px] md:h-[60px] h-[55px]",
+    },
+    {
+      name: "NodeJS",
+      logo: node,
+      cssClass: "lg:h-[80px] md:h-[60px] h-[55px]",
+    },
+    {
+      name: "Angular",
+      logo: angular,
+      cssClass: "lg:h-[80px] md:h-[60px] h-[55px]",
+    },
+    {
+      name: "jQuery",
+      logo: jquery,
+      cssClass: "lg:h-[80px] md:h-[60px] h-[55px]",
+    },
+    {
+      name: "WordPress",
+      logo: wordpress,
+      cssClass: "lg:h-[80px] md:h-[60px] h-[55px]",
+    },
+    {
+      name: "Bootstrap CSS",
+      logo: bootstrap,
+      cssClass: "lg:h-[80px] md:h-[60px] h-[55px]",
+    },
+    {
+      name: "Tailwind CSS",
+      logo: tailwind,
+      cssClass: "lg:h-[80px] md:h-[60px] h-[55px]",
+    },
+  ];
+
+  const dbs = [
+    {
+      name: "MS SQL",
+      logo: mssql,
+      cssClass: "lg:h-[80px] md:h-[60px] h-[55px]",
+    },
+    {
+      name: "MySQL",
+      logo: mysql,
+      cssClass: "lg:h-[80px] md:h-[60px] h-[55px]",
+    },
+    {
+      name: "PostgreSQL",
+      logo: postgresql,
+      cssClass: "lg:h-[80px] md:h-[60px] h-[55px]",
+    },
+    {
+      name: "MongoDB",
+      logo: mongodb,
+      cssClass: "lg:h-[80px] md:h-[60px] h-[55px]",
+    },
+    {
+      name: "MariaDB",
+      logo: maridb,
+      cssClass: "lg:h-[80px] md:h-[60px] h-[55px]",
+    },
+    {
+      name: "SQLite",
+      logo: sqlite,
+      cssClass: "lg:h-[80px] md:h-[60px] h-[55px]",
+    },
+  ];
+
   return (
     <>
       {/* TOP */}
@@ -122,80 +255,80 @@ function Landing() {
       </div>
 
       {/* PROGRAMMING SKILLS */}
-      <div className="flex flex-col w-full justify-center bg-gray-200 rounded-tr-[200px] rounded-bl-[200px] md:rounded-bl-[100px] lg:rounded-bl-[200px] h-full lg:h-screen md:h-screen lg:min-h-[500px] md:h-screen md:min-h-[100px] min-h-[700px] py-10 px-10">
+      <div className="flex flex-col w-full justify-center bg-gray-200 rounded-tr-[200px] rounded-bl-[200px] md:rounded-bl-[100px] lg:rounded-bl-[200px] h-full md:h-full lg:min-h-screen lg:min-h-[500px] md:min-h-[100px] min-h-[700px] py-10 px-10">
         <h1 className="text-3xl lg:text-center md:text-center lg:text-3xl md:text-2xl font-bold text-cyan-700 pt-3">
           My Expertise
         </h1>
 
-        <div className="flex-1 flex flex-col lg:flex-row md:flex-row pt-10 gap-5">
+        <div className="flex-1 flex flex-col lg:flex-row md:flex-row pt-10 gap-16">
+          {/* Languages */}
           <div className="flex-1 flex flex-col">
-            <p className="text-gray-800 text-opacity-50">
+            <p className="text-gray-800 text-center text-opacity-90">
               Programming Languages
             </p>
 
-            <div className="flex-1 flex flex-wrap justify-center pt-10 gap-5">
-              <img
-                src={javascript}
-                alt=""
-                className="lg:h-[120px] md:h-[90px] h-[60px]"
-              />
-              <img
-                src={css}
-                alt=""
-                className="lg:h-[120px] md:h-[90px] h-[60px]"
-              />
-              <img
-                src={html}
-                alt=""
-                className="lg:h-[120px] md:h-[90px] h-[60px]"
-              />
-              <img
-                src={php}
-                alt=""
-                className="lg:h-[90px] md:h-[70px] h-[50px]"
-              />
-              <img
-                src={java}
-                alt=""
-                className="lg:h-[120px] md:h-[90px] h-[60px]"
-              />
-              <img
-                src={python}
-                alt=""
-                className="lg:h-[120px] md:h-[90px] h-[60px]"
-              />
-              <img
-                src={flutter}
-                alt=""
-                className="lg:h-[120px] md:h-[90px] h-[60px]"
-              />
-              <img
-                src={c}
-                alt=""
-                className="lg:h-[120px] md:h-[90px] h-[60px]"
-              />
-              <img
-                src={csharp}
-                alt=""
-                className="lg:h-[120px] md:h-[90px] h-[60px]"
-              />
+            <div className="flex-1 flex flex-wrap justify-center pt-10 gap-x-8 gap-y-2">
+              {languages.map((icon) => (
+                <img
+                  src={icon.logo}
+                  alt={icon.name}
+                  className={icon.cssClass}
+                  title={icon.name}
+                />
+              ))}
             </div>
           </div>
 
+          {/* Frameworks */}
           <div className="flex-1 flex flex-col">
-            <p className="text-gray-800 text-opacity-50">
+            <p className="text-gray-800 text-center text-opacity-90">
               Frameworks and Libraries
             </p>
+
+            <div className="flex-1 flex flex-wrap justify-center pt-10 gap-x-8 gap-y-2">
+              {frameworks.map((icon) => (
+                <img
+                  src={icon.logo}
+                  alt={icon.name}
+                  className={icon.cssClass}
+                  title={icon.name}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div className="flex-1 flex flex-col lg:flex-row md:flex-row pt-10 gap-16">
+          {/* Backend Databases */}
+          <div className="flex-1 flex flex-col">
+            <p className="text-gray-800 text-center text-opacity-90">
+              Backend Databases
+            </p>
+
+            <div className="flex-1 flex flex-wrap justify-center pt-10 gap-x-8 gap-y-2">
+              {dbs.map((icon) => (
+                <img
+                  src={icon.logo}
+                  alt={icon.name}
+                  className={icon.cssClass}
+                  title={icon.name}
+                />
+              ))}
+            </div>
           </div>
         </div>
       </div>
+
+      {/* INSTITUTIONAL BACKGROUND */}
+
+      {/* <div className="flex flex-col w-full justify-center h-full md:h-full lg:min-h-screen lg:min-h-[500px] md:min-h-[100px] min-h-[700px] py-10 px-10"></div> */}
 
       {/* VFX */}
       <div className="flex items-center justify-center">
         <div className="relative overflow-hidden bg-black rounded-tr-[200px] rounded-bl-[200px] md:rounded-bl-[100px] lg:rounded-bl-[200px] h-full lg:h-screen md:h-screen lg:min-h-[500px] md:h-screen md:min-h-[100px] min-h-[700px] py-3">
           <video
             className="absolute top-0 left-0 w-full h-full object-cover opacity-50"
-            src="/vfx/pf-bg.mov" // Update the path to your video
+            src="/vfx/pf-bg.mp4" // Update the path to your video
             autoPlay
             loop
             muted
@@ -217,11 +350,11 @@ function Landing() {
                 <p className="pt-4 text-white">
                   With 7+ years of experience in 3D modeling, sculpting,
                   rigging, texturing, and animation in Blender, Substance
-                  Painter, ZBrush, and Maya, and 6+ years of expering in visual
-                  effects with Blender, Houdini, Cinema4D, and Adobe After
-                  Effects. Worked with several local and international companies
-                  in creating artistic and realistic CGI advertisements, short
-                  clips, and even movies.
+                  Painter, ZBrush, and Maya, and 6+ years of experience in
+                  visual effects with Blender, Houdini, Cinema4D, and Adobe
+                  After Effects. Worked with several local and international
+                  companies in creating artistic and realistic CGI
+                  advertisements, short clips, and even movies.
                 </p>
                 <p className="pt-3 text-white">
                   I have established my fanbase in <strong>TikTok</strong>,
